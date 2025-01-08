8991aff2e078a86832bfa5983370b8c94731d6cd
Gerson <128327533+G3RSONV@users.noreply.github.com>
2025-01-07 20:47:21 -0500

    added windows server 2019 vm in terraform main.tf and updated the vm_inventory.md

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,10 +20,13 @@ provider "proxmox" {
   }
 }
 
+
+# ubuntu server vm
 resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   name        = "ubuntu-server"
   node_name = "pve"
   vm_id = 102
+  started = false
 
   agent {
     enabled = false
@@ -36,19 +39,24 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
     }
     ip_config {
       ipv4 {
-        address = "dhcp"
+        address = "192.168.86.131/24"
       }
     }
   }  
 
   disk {
     datastore_id = "extrassd"
-    file_id = "local:iso/ubuntu-24.04.1-live-server-amd64.iso"
+    file_format = "raw"
     interface = "virtio0"
     iothread = true
     size = 20
    }
   
+  cdrom {
+    enabled = true
+    file_id = "local:iso/ubuntu-24.04.1-live-server-amd64.iso"
+  }
+
   cpu {
     cores = 2
     type = "x86-64-v2-AES"
@@ -67,33 +75,64 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
     type = "l26"
   }
  
-  serial_device {}
+  # serial_device {}
 }    
-#   network {
-#     model  = "virtio"
-#     bridge = "vmbr0"
-#   }
-#   iso = 
-#   os_type = "linux"
-#   ssh_keys = file("~/.ssh/id_rsa.pub") # Path to your SSH public key
-# }
 
-# resource "proxmox_virtual_environment_vm" "windows_vm" {
-#   name      = "windows-server"
-#   target_node = "proxmox-node-1"
-#   cores     = 2
-#   memory    = 4096
-#   disk {
-#     size = "50G"
-#     type = "scsi"
-#   }
-#   network {
-#     model  = "virtio"
-#     bridge = "vmbr0"
-#   }
-#   iso = "local:iso/windowsserver.iso"
-#   os_type = "win10"  # Use 'win10' for Windows Server ISO as well
-#   winrm_password = var.wserver_psswd
-# }
+# windows server vm
+resource "proxmox_virtual_environment_vm" "windows_vm" {
+  name        = "windows-server"
+  node_name = "pve"
+  vm_id = 103
+  started = false
+
+  agent {
+    enabled = false
+  }
+  
+  initialization {
+    user_account {
+      username = "admin"
+      password = "password"
+    }
+    ip_config {
+      ipv4 {
+        address = "192.168.86.132/24"
+      }
+    }
+  }  
+
+  disk {
+    datastore_id = "extrassd"
+    # file_id = "local:iso/windowsserver.iso"
+    interface = "sata0"
+    file_format = "raw"
+    # iothread = true
+    size = 32
+   }
+  
+  cdrom {
+    enabled = true
+    file_id = "local:iso/windowsserver.iso"
+  }
+
+  cpu {
+    cores = 2
+    type = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated = 2048
+    floating  = 2048 # set equal to dedicated to enable ballooning
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  operating_system {
+    type = "win10"
+  }
 
 
+  
+}    
